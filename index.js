5c198b99c7086e83b3188cb1716e758efbd2b357
Fedor Tatarintsev <91975849+thriwax@users.noreply.github.com>
2022-12-01 07:54:27 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions } = require('discord.js');
+
+const prefix = '-';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
+
+const link =
+  "https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=AJNUI74A3Q7ASVN6J7EKG6NBZSBSCP8X14";
+
+const fetchData = async () => {
+  const result = await fetch(link);
+  const data = await result.json();
+
+  const {
+    result: {suggestBaseFee, SafeGasPrice, ProposeGasPrice, FastGasPrice}
+  } = data
+
+  const total = [suggestBaseFee, SafeGasPrice, ProposeGasPrice, FastGasPrice]
+  return total
+};
+
+fetchData();
+
+
+client.on('ready', () => {
+    console.log('Bot is online!');
+
+    client.user.setActivity(`Subscribe to Mr Template`, { type: 'WATCHING'});
+
+})
+
+client.on('messageCreate', async (message) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(' ');
+    const command = args.shift().toLowerCase();
+
+    const messageArray = message.content.split(' ');
+    const argument = messageArray.slice(1);
+    const cmd = messageArray[0];
+
+
+if (command === 'gas') {
+    const basefee = await fetchData()
+    console.log(basefee);
+    message.channel.send(
+`
+Ethereum Gas Tracker:
+
+⛽ Gas Price Now: ${basefee[3]} GWEI
+🧾 Base Fee: ${basefee[0]} GWEI`)
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login('MTA0MDg4ODc0Nzc1Mjk1MTgxOQ.GGNNwt.CikYmTt8HhCAVgilQAIi7SIxnNA84U1bfZ0-Kc')
